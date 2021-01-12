18de6d94111a819884fe06bbce19fe9265661ee6
pcmt93 <matos.p@pucp.pe>
2021-01-11 23:58:01 -0500

    Correction: add input address, improve iteration

--- code/geo_code.py
+++ code/geo_code.py
@@ -3,6 +3,8 @@
 """
 Created on Wed Aug 29 19:02:46 2018
 
+Modified: Jan11/2020
+
 @author: paulomatos
 """
 
@@ -15,7 +17,7 @@ import os
 
 # Define your working directory here:
     
-db = "/Users/paulomatos/Dropbox/Repos/ExGoogleMapAPI"
+db = "/Users/ptrifu/Dropbox/datos-incarceration/data/interim"
 os.chdir(db)
 
 # Import needed modules 
@@ -24,7 +26,6 @@ import pandas as pd
 import requests
 import logging
 import time
-
 
 #---------------------------------------------------------------
 # configuration 
@@ -39,24 +40,25 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 logger.addHandler(ch)
 
-API_KEY = "" # You need to get an API key from google cloud. There is 
+API_KEY = "" 
+    # You need to get an API key from google cloud. There is 
     # one free trial that allows you to geocode around 100k addresses
 
 BACKOFF_TIME = 30 # because now there is no limit per day, just per second 
-# rest a minute, que can vary it 
+# rest a minute, it is possible to vary it 
 
 # Output filename here 
 
-output_filename = 'geo_12588_pm.csv'
+output_filename = 'prueba_output.xlsx'
 
 # Input filename 
-1
-input_filename = 'direcciones12588_pm.csv'
+
+input_filename = 'prueba.xlsx'
 
 # Column name that contains address
 
-address_column_name = 'dir1_completa'
-id_dataframe = 'codigoempresa'
+address_column_name = 'address'
+id_dataframe = 'uid'
 
 # Return Full Google results, could be useful
 
@@ -66,19 +68,13 @@ RETURN_FULL_RESULTS = False
 
 # Read the data 
 
-slv = pd.read_csv(input_filename, encoding = 'utf8')
+df = pd.read_excel(input_filename)
 
-if address_column_name not in slv.columns:
+if address_column_name not in df.columns:
     raise ValueError('Missing Address column in input data')
     
-addresses = slv[address_column_name] ## only matters the address turn to 
-# dataframe to object type 
 
-# dataframe to object type 
-id = slv[id_dataframe]
-
-
-#--------------------------------------------------------------#
+#---------------------------------------------------------------#
 
 # funtion definitions 
 
@@ -94,6 +90,8 @@ def get_google_results(address, api_key = None, return_full_response = False):
     As mentioned before api_key = None, only 2500 cases per day
     
     """
+    
+    # To put in format the key:
 
     geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?address={}".format(address)
 
@@ -102,7 +100,7 @@ def get_google_results(address, api_key = None, return_full_response = False):
         geocode_url = geocode_url + "&key={}".format(api_key) ## just to keep
         ## it general, if in case we get an api key
     
-    # Pingo google for the results
+    # google for the results
     
     results = requests.get(geocode_url)
     
@@ -137,19 +135,10 @@ def get_google_results(address, api_key = None, return_full_response = False):
             "postcode": ",".join([x['long_name'] for x in answer.get('address_components') 
                                   if 'postal_code' in x.get('types')])
                 
-                
-                
-                }
-    
-    # Append some other details:
-    
-    output['id'] = id
-    output['input_string'] = address
-    output['number_of_results'] = len(results['results'])
-    output['status'] = results.get('status')
-    
-    if return_full_response is True:
-        output['response'] = results
+        }
+
+
+    output['status'] = results.get('status') # to get status of the query
     
     return output
     
@@ -161,7 +150,8 @@ results = [] # list
 
 # Go to each address in the list of addresses 
 
-for address in addresses:
+
+for ind in df.index:
     
     # While the address geocoding is not finished:
     
@@ -172,18 +162,20 @@ for address in addresses:
         # Geocode the address with google
         
         try:
-            geocode_result = get_google_results(address, API_KEY,
-                                                return_full_response=RETURN_FULL_RESULTS)
+            geocode_result = get_google_results(df[address_column_name][ind], 
+                                                API_KEY, 
+                                                return_full_response = 
+                                                RETURN_FULL_RESULTS)
         except Exception as e:
             logger.exception(e)
-            logger.error("Major error with {}".format(address))
+            logger.error("Major error with {}".format(df[address_column_name][ind]))
             logger.error("Skipping!")
             geocoded = True
             
         # If we're over the API limit, backoff for a while and try again later.
         if geocode_result['status'] == 'OVER_QUERY_LIMIT':
             logger.info("Hit Query Limit! Backing off for a bit.")
-            time.sleep(BACKOFF_TIME) # sleep for xx minutes
+            time.sleep(BACKOFF_TIME) # Re-try in X seconds
             geocoded = False
             
         else:
@@ -191,17 +183,30 @@ for address in addresses:
             # If we're ok with API use, save the results
             # Note that the results might be empty / non-ok - log this
             if geocode_result['status'] != 'OK':
-                logger.warning("Error geocoding {}: {}".format(address, geocode_result['status']))
-            logger.debug("Geocoded: {}: {}".format(address, geocode_result['status']))
+                logger.warning("Error geocoding {}: {}".format(df[address_column_name][ind], 
+                                                               geocode_result['status']))
+            
+            logger.debug("Geocoded: {}: {}".format(df[address_column_name][ind], 
+                                                   geocode_result['status']))
+            
+            # Before appending, I am going to add the columns of the input
+            # to the new dataset 
+            
+            geocode_result[id_dataframe] = df[id_dataframe][ind]
+            geocode_result['input_address'] = df[address_column_name][ind]
+            
             results.append(geocode_result)           
             geocoded = True
+            
+            
+    # Finally, I am going to append to the output database the 
             
             
     # Print status every 100 addresses 
     
     if len(results) % 100 == 0:
         logger.info("Completed {} of {} address".format(len(results), 
-                    len(addresses)))
+                    len(df[address_column_name])))
     
     # Every 500 addresses, save progress to file (just in case :) )
     
@@ -213,7 +218,7 @@ for address in addresses:
 logger.info("Finished geocoding all addresses")
 # Write the full results to csv using the pandas library.
 
-pd.DataFrame(results).to_csv(output_filename, encoding='utf8')
+pd.DataFrame(results).to_excel(output_filename, encoding='utf8')
 
 
 #----------------------------------------------------------------------------
